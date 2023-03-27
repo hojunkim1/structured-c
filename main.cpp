7ec9cdd67bcea5113a20dd6e143b0d6602a20248
Hojun Kim <hojunkim02@icloud.com>
2023-03-27 15:26:29 +0900

    assert example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#define NDEBUG
+#include <cassert>
+using namespace std;
+
+void show_number(int *ptr)
+{
+    assert(ptr != NULL);
+    cout << "The number is: " << *ptr << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x;
+
+    int *first_ptr = NULL;
+    int *second_ptr = NULL;
+
+    x = 10;
+    first_ptr = &x;
+
+    show_number(first_ptr);
+    show_number(second_ptr);
+
     return 0;
 }
