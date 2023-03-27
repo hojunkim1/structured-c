54eefca91a4f73e4f266607afdb966891bce447c
Hojun Kim <hojunkim02@icloud.com>
2023-03-27 15:38:30 +0900

    add gitignore

--- main.cpp
+++ main.cpp
@@ -1,26 +1,12 @@
-#include <iostream>
-#define NDEBUG
-#include <cassert>
-using namespace std;
+#define DEBUG
 
-void show_number(int *ptr)
-{
-    assert(ptr != NULL);
-    cout << "The number is: " << *ptr << endl;
-}
+#include <iostream>
 
 int main()
 {
-    int x;
-
-    int *first_ptr = NULL;
-    int *second_ptr = NULL;
-
-    x = 10;
-    first_ptr = &x;
-
-    show_number(first_ptr);
-    show_number(second_ptr);
-
+#ifdef DEBUG
+    std::cout << "Debug mode" << std::endl;
+#endif
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
